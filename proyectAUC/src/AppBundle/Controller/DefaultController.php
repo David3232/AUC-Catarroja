a728d70f5f209e7dcd8f34b449d3f4303a1b08cf
sergioruiz <sergio_311096@hotmail.es>
2019-02-06 09:35:27 +0100

    subir css y js para formulario

--- proyectAUC/src/AppBundle/Controller/DefaultController.php
+++ proyectAUC/src/AppBundle/Controller/DefaultController.php
@@ -18,4 +18,14 @@ class DefaultController extends Controller
             'base_dir' => realpath($this->getParameter('kernel.project_dir')).DIRECTORY_SEPARATOR,
         ]);
     }
+    /**
+     * @Route("/pruebaForm", name="pruebaForm")
+     */
+    public function pruebaForm(Request $request)
+    {
+        // replace this example code with whatever you need
+        return $this->render('form.html.twig', [
+            'base_dir' => realpath($this->getParameter('kernel.project_dir')).DIRECTORY_SEPARATOR,
+        ]);
+    }
 }
